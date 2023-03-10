c4e6dc8e8806afaf5e07ae9b89e164e107131b44
val-fr69 <[valentin.inacio@live.fr](mailto:valentin.inacio@live.fr)>
2023-03-10 17:19:54 +0100

    modification navbar

--- script.js
+++ script.js
@@ -6,8 +6,3 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
