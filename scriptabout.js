1290ce3ca520ef7e620ff730cb069745f4917d7a
Mouhamed <mouhamediop114@gmail.com>
2023-03-17 15:37:32 +0100

    premier commit ajout page About

---
+++ scriptabout.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const titleHome = document.querySelector('.home-head');
+const nav = document.querySelector('.nav-bar');
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    titleHome.classList.toggle('active');
+})
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 160) {
+        nav.classList.add('scroll');
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        titleHome.classList.add('scroll');
+    }
+    else {
+        nav.classList.remove('scroll');
+        titleHome.classList.remove('scroll');
+    }
+});
